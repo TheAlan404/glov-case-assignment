8fe73ea6fa0a685919bd57e3b37c2c37e404baa5
TheAlan404 <gokdenizbektas@gmail.com>
2024-08-22 21:41:33 +0300

    ChatMessage: add avatar

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,5 +1,6 @@
-import { Image, Paper, SimpleGrid, Stack, Text } from "@mantine/core";
+import { Avatar, Group, Image, Paper, SimpleGrid, Stack, Text } from "@mantine/core";
 import { Message } from "./types/message"
+import { IconMessageChatbot, IconUser } from "@tabler/icons-react";
 
 export const ChatMessage = ({
     message
@@ -13,25 +14,38 @@ export const ChatMessage = ({
             withBorder
             shadow="md"
         >
-            <Stack gap={0}>
-                <Text fw="bold">
-                    {/* TODO: Replace with username */}
-                    {message.sender == "user" ? "You" : "Chat Agent"}
-                </Text>
-                <Text>
-                    {message.content}
-                </Text>
-                {!!message.attachments?.length && (
-                    <SimpleGrid cols={message.attachments.length == 1 ? 1 : 2}>
-                        {message.attachments.map((attachment, i) => (
-                            <Image
-                                key={i}
-                                src={attachment.url}
-                            />
-                        ))}
-                    </SimpleGrid>
-                )}
-            </Stack>
+            <Group wrap="nowrap">
+                <Stack>
+                    <Avatar
+                        color={message.sender == "user" ? "blue" : "gray"}
+                    >
+                        {message.sender == "user" ? (
+                            <IconUser />
+                        ) : (
+                            <IconMessageChatbot />
+                        )}
+                    </Avatar>
+                </Stack>
+                <Stack gap={0}>
+                    <Text fw="bold">
+                        {/* TODO: Replace with username */}
+                        {message.sender == "user" ? "You" : "Chat Agent"}
+                    </Text>
+                    <Text>
+                        {message.content}
+                    </Text>
+                    {!!message.attachments?.length && (
+                        <SimpleGrid cols={message.attachments.length == 1 ? 1 : 2}>
+                            {message.attachments.map((attachment, i) => (
+                                <Image
+                                    key={i}
+                                    src={attachment.url}
+                                />
+                            ))}
+                        </SimpleGrid>
+                    )}
+                </Stack>
+            </Group>
         </Paper>
     )
 }
