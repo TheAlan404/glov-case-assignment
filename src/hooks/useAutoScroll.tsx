02247bbdd458d87e60bffe511f2d095ec7f5fde1
TheAlan404 <gokdenizbektas@gmail.com>
2024-08-23 20:41:44 +0300

    useAutoScroll: initial scroll

--- src/hooks/useAutoScroll.tsx
+++ src/hooks/useAutoScroll.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 
 export const useAutoScroll = (deps: React.DependencyList) => {
     const ref = useRef<HTMLDivElement | null>(null);
-    const atBottom = useRef(false);
+    const atBottom = useRef(true);
 
     useEffect(() => {
         if(ref.current && atBottom.current) {
@@ -15,8 +15,10 @@ export const useAutoScroll = (deps: React.DependencyList) => {
 
         const onScroll = () => {
             if(!ref.current) return;
-            atBottom.current = Math.abs(ref.current.scrollHeight - ref.current.clientHeight - ref.current.scrollTop) <= 1;
+            atBottom.current = ref.current.scrollHeight == ref.current.clientHeight || Math.abs(ref.current.scrollHeight - ref.current.clientHeight - ref.current.scrollTop) <= 1;
         };
+
+        ref.current?.scrollTo({ top: ref.current?.scrollHeight });
 
         ref.current.addEventListener("scroll", onScroll);
 
